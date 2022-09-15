11184f4a4913246caacbe415b485c81d58111350
Caio <caio.balbi@compasso.com.br>
2022-09-14 21:54:50 -0400

    refactor: activity description type

--- lib/model/activity_model.dart
+++ lib/model/activity_model.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class Activity {
   late String title;
-  String? description;
+  String description;
   late DateTime initialDatetime;
   DateTime? finalDatetime;
   late DateTime createdAt;
@@ -10,7 +10,7 @@ class Activity {
 
   Activity({
     required this.title,
-    this.description,
+    this.description = "",
     required this.initialDatetime,
     this.finalDatetime,
     required this.createdAt,
