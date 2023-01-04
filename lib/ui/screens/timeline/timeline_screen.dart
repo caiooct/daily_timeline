e73017ab7b00a029bf6bccd22b2267e073330d2f
Caio <pkaiow@gmail.com>
2023-01-03 22:46:18 -0400

    refactor: refact timeline screen
    - refactor title's text style in activity card
    - add appbar and bottomappbar
    - separate components in private widgets

--- lib/ui/screens/timeline/timeline_screen.dart
+++ lib/ui/screens/timeline/timeline_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../../../settings/datetime_extension.dart';
 
 import '../../../models/activity_model.dart';
 import '../../../settings/activities_mock.dart';
@@ -13,14 +15,46 @@ class TimelineScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: const EdgeInsets.all(8),
-      itemCount: activitiesList.length + 2,
-      itemBuilder: (context, idx) {
-        if (idx == 0) return _buildInitialWidget();
-        if (idx == activitiesListMock.length + 1) return _buildFinalWidget();
-        return _buildListItem(activitiesListMock[idx - 1], context);
-      },
+    DateTime today = DateTime.now();
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.arrow_back_ios,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              DateFormat.yMMMM().format(today),
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge
+                  ?.copyWith(color: Colors.white),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+        bottom: const _BottomAppBar(),
+      ),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: activitiesList.length + 2,
+        itemBuilder: (context, idx) {
+          if (idx == 0) return _buildInitialWidget();
+          if (idx == activitiesListMock.length + 1) return _buildFinalWidget();
+          return _ActivityItemWidget(activity: activitiesList[idx - 1]);
+        },
+      ),
     );
   }
 
@@ -60,8 +94,72 @@ class TimelineScreen extends StatelessWidget {
       ),
     );
   }
+}
 
-  Widget _buildListItem(Activity activity, context) {
+class _BottomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const _BottomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    DateTime today = DateTime.now();
+    DateTime iterableDay = today.subtract(Duration(days: today.weekday));
+    return IntrinsicHeight(
+      child: Row(
+        children: List.generate(
+          DateTime.daysPerWeek,
+          (_) {
+            bool isToday = iterableDay.isOnSameDayAs(today);
+            Widget textWidget = Text(
+              "${iterableDay.day}",
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                color: isToday ? primary : Colors.white,
+              ),
+            );
+            if (iterableDay.isOnSameDayAs(today)) {
+              textWidget = Flexible(
+                flex: 1,
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  maxRadius: 12,
+                  child: textWidget,
+                ),
+              );
+            }
+            final widget = Expanded(
+              child: Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.only(bottom: 10.0),
+                child: Column(
+                  children: [
+                    Text(
+                      DateFormat.E().format(iterableDay),
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                    textWidget,
+                  ],
+                ),
+              ),
+            );
+            iterableDay = iterableDay.add(const Duration(days: 1));
+            return widget;
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size(double.infinity, 64.0);
+}
+
+class _ActivityItemWidget extends StatelessWidget {
+  final Activity activity;
+
+  const _ActivityItemWidget({required this.activity});
+
+  @override
+  Widget build(BuildContext context) {
     return IntrinsicHeight(
       child: Row(
         mainAxisAlignment: MainAxisAlignment.start,
@@ -69,58 +167,55 @@ class TimelineScreen extends StatelessWidget {
         children: [
           Column(
             children: [
-              _buildActivityIcon(activity.icon),
+              Container(
+                width: 48,
+                height: 48,
+                decoration: const ShapeDecoration(
+                    shape: CircleBorder(), color: primary),
+                child: Center(
+                    child: Icon(activity.icon, color: Colors.white, size: 30)),
+              ),
               Expanded(child: Container(color: primary, width: 5)),
             ],
           ),
           const SizedBox(width: 8.0),
-          Expanded(child: _buildActivityItem(activity, context)),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildActivityIcon(IconData icon) {
-    return Container(
-      width: 48,
-      height: 48,
-      decoration: const ShapeDecoration(shape: CircleBorder(), color: primary),
-      child: Center(child: Icon(icon, color: Colors.white, size: 30)),
-    );
-  }
-
-  Widget _buildActivityItem(Activity activity, BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(left: 8.0, top: 4.0),
-          child: _buildHour(activity, context),
-        ),
-        Row(children: [Expanded(child: ActivityCard(activity: activity))]),
-        const SizedBox(height: 32),
-      ],
-    );
-  }
-
-  Widget _buildHour(Activity activity, BuildContext context) {
-    return RichText(
-      textAlign: TextAlign.center,
-      text: TextSpan(
-        children: [
-          TextSpan(
-            text:
-                "${activity.initialDatetime.hour}:${activity.initialDatetime.minute.toString().padRight(2, '0')}\n",
-            style: Theme.of(context)
-                .textTheme
-                .headline6
-                ?.copyWith(color: primary, fontWeight: FontWeight.bold),
-          ),
-          TextSpan(
-            text:
-                "${activity.initialDatetime.hour}:${activity.initialDatetime.minute.toString().padRight(2, '0')} - ${activity.finalDatetime!.hour}:${activity.finalDatetime!.minute.toString().padRight(2, '0')}",
-            style:
-                Theme.of(context).textTheme.bodyText1?.copyWith(color: primary),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0, top: 4.0),
+                  child: RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text:
+                              "${DateFormat.jm().format(activity.initialDatetime).toLowerCase()}\n",
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleLarge
+                              ?.copyWith(
+                                  color: primary, fontWeight: FontWeight.bold),
+                        ),
+                        TextSpan(
+                          text:
+                              "${DateFormat.jm().format(activity.initialDatetime).toLowerCase()} - ${DateFormat.jm().format(activity.finalDatetime!).toLowerCase()}",
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleLarge
+                              ?.copyWith(color: primary),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Row(
+                  children: [Expanded(child: ActivityCard(activity: activity))],
+                ),
+                const SizedBox(height: 32),
+              ],
+            ),
           ),
         ],
       ),
--- lib/ui/screens/timeline/widgets/activity_card_widget.dart
+++ lib/ui/screens/timeline/widgets/activity_card_widget.dart
@@ -19,7 +19,13 @@ class ActivityCard extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(activity.title, style: Theme.of(context).textTheme.headline6),
+            Text(
+              activity.title,
+              style: Theme.of(context)
+                  .textTheme
+                  .titleMedium
+                  ?.copyWith(fontWeight: FontWeight.bold),
+            ),
             const SizedBox(height: 8),
             if (activity.description.isNotEmpty)
               Text(
